686cdb617e562decf7fca5194d6e8d7a27b58820
lorenzog96 <109627171+lorenzog96@users.noreply.github.com>
2022-07-20 11:37:22 +0200

    Create R_CODE_calcolo_index.r

---
+++ R_CODE_calcolo_index.r
@@ -0,0 +1,10 @@
+# richiamo le librerie
+library(raster)
+library(RStoolbox)
+
+# importo il primo file, attribuendolo ad un oggetto per semplificare i prossimi passaggi
+l1992 <- brick("defor1_.jpg")
+
+
+
+
