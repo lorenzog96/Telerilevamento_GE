d204ff221cb1929b74646c6c7100b6f2803a406a
lorenzog96 <109627171+lorenzog96@users.noreply.github.com>
2022-07-22 13:41:37 +0200

    Update R_CODE_exam.r

--- R_CODE_exam.r
+++ R_CODE_exam.r
@@ -1,3 +1,5 @@
+# Carico tutti i pacchetti necessari per le varie funzioni che andrò ad effettuare
+
 library(raster)
 library(RStoolbox)
 library(rasterdiv)
@@ -6,5 +8,20 @@ library(patchwork)
 library(viridis)
 library(sdm)
 
+# Carico la cartella da cui verranno prelevati i file
+
 setwd("C:/Users/Lorenzo/Desktop/exam")
+
+# Carico tutti i file necessari per lo studio della situazione del 1985
+
+b5 <- brick("1985_B1.TIF")
+g5 <- brick("1985_B2.TIF")
+r5 <- brick("1985_B3.TIF")
+i5 <- brick("1985_B4.TIF")
+l5 <- brick("1985_B5.TIF")
+t5 <- brick("1985_B6.TIF")
+m5 <- brick("1985_B7.TIF")
+
+
+
 
