5ec2fc45f94383df91568d97caf52c8181eae198
lorenzog96 <109627171+lorenzog96@users.noreply.github.com>
2022-07-23 11:29:21 +0200

    Update R_CODE_exam.r

--- R_CODE_exam.r
+++ R_CODE_exam.r
@@ -38,10 +38,6 @@ names      : L1C_T32TQQ_A028033_20220719T095747.1, L1C_T32TQQ_A028033_20220719T0
 min values :                                    0,                                    0,                                    0 
 max values :                                  255,                                  255,                                  255 
 
-# Vado a plottare attraverso la funzione plot RGB
-r15 <- plotRGB(i15, r=1, g=2, b=3, stretch="lin")
-r22 <- plotRGB(i22, r=1, g=2, b=3, stretch="lin")
-
 # Creo un plot con un singolo multiframe con la funzione ggRGB
 a15 <- ggRGB(i15, r=1, g=2, b=3, stretch="lin")
 a22 <- ggRGB(i22, r=1, g=2, b=3, stretch="lin")
@@ -105,6 +101,111 @@ Data <- data.frame(Class, Perc_2015, Perc_2022)
 
 # Dai dati risulta che la quantità di area ricoperta da acqua
 # sia aumentata negli ultimi anni.
+
+# Vado ora a calcolare la variabilità delle due situazioni,
+# utilizzando il metodo di discriminazione tramite l'Analisi Multivariata
+
+# Utilizzo la funzione rasterPCA per effettuarne l'analisi
+i15PCA <- rasterPCA(i15)
+i22PCA <- rasterPCA(i22)
+
+# Informazioni legate a i15PCA:
+$call
+rasterPCA(img = i15)
+
+$model
+Call:
+princomp(cor = spca, covmat = covMat[[1]])
+
+Standard deviations:
+   Comp.1    Comp.2    Comp.3 
+84.561689 14.828571  5.877526 
+
+ 3  variables and  19260621 observations.
+
+$map
+class      : RasterBrick 
+dimensions : 5490, 5490, 30140100, 3  (nrow, ncol, ncell, nlayers)
+resolution : 20, 20  (x, y)
+extent     : 699960, 809760, 4890240, 5000040  (xmin, xmax, ymin, ymax)
+crs        : +proj=utm +zone=32 +datum=WGS84 +units=m +no_defs 
+source     : r_tmp_2022-07-23_095948_9376_21083.grd 
+names      :        PC1,        PC2,        PC3 
+min values :  -97.80288, -123.65265, -130.84856 
+max values :  306.77044,   99.79026,   55.19634 
+
+
+attr(,"class")
+[1] "rasterPCA" "RStoolbox"
+
+# Informazioni legate a i22PCA:
+$call
+rasterPCA(img = i22)
+
+$model
+Call:
+princomp(cor = spca, covmat = covMat[[1]])
+
+Standard deviations:
+   Comp.1    Comp.2    Comp.3 
+88.105638 14.255595  5.465312 
+
+ 3  variables and  19552576 observations.
+
+$map
+class      : RasterBrick 
+dimensions : 5490, 5490, 30140100, 3  (nrow, ncol, ncell, nlayers)
+resolution : 20, 20  (x, y)
+extent     : 699960, 809760, 4890240, 5000040  (xmin, xmax, ymin, ymax)
+crs        : +proj=utm +zone=32 +datum=WGS84 +units=m +no_defs 
+source     : r_tmp_2022-07-23_102459_15364_18954.grd 
+names      :       PC1,       PC2,       PC3 
+min values : -112.6638, -161.0792, -135.2638 
+max values :  296.2612,  105.1434,  105.3539 
+
+
+attr(,"class")
+[1] "rasterPCA" "RStoolbox"
+
+# Vado ora a visualizzare il peso di ogni banda per le due situazioni dell'area:
+summary(i15PCA$model)
+
+# Info su i15
+Importance of components:
+                           Comp.1      Comp.2      Comp.3
+Standard deviation     84.5616892 14.82857122 5.877525833
+Proportion of Variance  0.9656411  0.02969389 0.004665063
+Cumulative Proportion   0.9656411  0.99533494 1.000000000
+
+# Si nota che con le prime due componenti si riesce già a spiegare il 98% circa della situazione del 2015.
+
+summary(i22PCA$model)
+
+# Info su i22
+Importance of components:
+                           Comp.1      Comp.2      Comp.3
+Standard deviation     88.1056380 14.25559534 5.465311797
+Proportion of Variance  0.9708479  0.02541643 0.003735714
+Cumulative Proportion   0.9708479  0.99626429 1.000000000
+
+# Anche in questo caso si riesce a spiegare il 99% circa dell'immagine attraverso le prime due componenti.
+
+# Plottando le due immagini satellitari suddivise per componenti si
+# riesce a confermare le osservazioni effettuate con il calcolo dell'analisi multivariata.
+
+plot(i15PCA$map)
+plot(i22PCA$map)
+
+# Vado ora a calcolare la deviazione standard delle due situazioni
+# utilizzando solamente la prima banda PC1, che ne spiega almeno il 95%.
+
+# Sulla situazione del 2015
+p1c5 <- i15PCA$map$PC1
+sd15 <- focal(p1c5, matrix(1/9, 3, 3), fun=sd)
+
+# Sulla situazione del 2022
+p1c2 <- i22PCA$map$PC1
+sd22 <- focal(p1c2, matrix(1/9, 3, 3), fun=sd)
 
 
 
