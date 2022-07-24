f94ffe97dc16708aa02328b84338b2c8780de8a2
lorenzog96 <109627171+lorenzog96@users.noreply.github.com>
2022-07-24 11:35:51 +0200

    Update R_CODE_exam.r

--- R_CODE_exam.r
+++ R_CODE_exam.r
@@ -29,6 +29,7 @@ ilist <- list.files(pattern="T32TQQ_")
 [7] "T32TQQ_20220717T100611_B07_20m.jp2"
 [8] "T32TQQ_20220717T100611_B11_20m.jp2"
 [9] "T32TQQ_20220717T100611_B12_20m.jp2"
+[10] "T32TQQ_20220717T100611_B8A_20m.jp2"
 
 # Vado ad importare la lista di file, conferendo come oggetto "podelta"
 podelta <- lapply(ilist, raster)
@@ -38,73 +39,73 @@ delta <- stack(podelta)
 
 # Info "delta"
 class      : RasterStack 
-dimensions : 5490, 5490, 30140100, 9  (nrow, ncol, ncell, nlayers)
+dimensions : 5490, 5490, 30140100, 10  (nrow, ncol, ncell, nlayers)
 resolution : 20, 20  (x, y)
 extent     : 699960, 809760, 4890240, 5000040  (xmin, xmax, ymin, ymax)
 crs        : +proj=utm +zone=32 +datum=WGS84 +units=m +no_defs 
-names      : T32TQQ_20//11_B01_20m, T32TQQ_20//11_B02_20m, T32TQQ_20//11_B03_20m, T32TQQ_20//11_B04_20m, T32TQQ_20//11_B05_20m, T32TQQ_20//11_B06_20m, T32TQQ_20//11_B07_20m, T32TQQ_20//11_B11_20m, T32TQQ_20//11_B12_20m 
-min values :                     0,                     0,                     0,                     0,                     0,                     0,                     0,                     0,                     0 
-max values :                 65535,                 65535,                 65535,                 65535,                 65535,                 65535,                 65535,                 65535,                 65535  
+names      : T32TQQ_20//11_B01_20m, T32TQQ_20//11_B02_20m, T32TQQ_20//11_B03_20m, T32TQQ_20//11_B04_20m, T32TQQ_20//11_B05_20m, T32TQQ_20//11_B06_20m, T32TQQ_20//11_B07_20m, T32TQQ_20//11_B11_20m, T32TQQ_20//11_B12_20m, T32TQQ_20//11_B8A_20m 
+min values :                     0,                     0,                     0,                     0,                     0,                     0,                     0,                     0,                     0,                     0 
+max values :                 65535,                 65535,                 65535,                 65535,                 65535,                 65535,                 65535,                 65535,                 65535,                 65535 
 
-# In questo modo i 9 file sono diventati un'unica immagine.
+# In questo modo i 10 file sono diventati un'unica immagine.
 
 # Effettuo il plot dell'immagine satellitare che ne deriva.
-plotRGB(delta, r=4, g=2, b=1, stretch="lin")
+plotRGB(delta, r=4, g=3, b=2, stretch="lin")
 
 # Creo un pdf del plot
 pdf("delta.pdf")
-plotRGB(delta, r=4, g=2, b=1, stretch="lin")
+plotRGB(delta, r=4, g=3, b=2, stretch="lin")
 
 # Con questa modalità si vede l'immagine nel visibile (come la vedremmo con i nostri occhi).
-# Layer 1 = Blue
-# Layer 2 = Green
-# Layer 3 = NIR
+# Layer 2 = Blue
+# Layer 3 = Green
 # Layer 4 = Red
+# Layer 5 = NIR
 
 # Creo le classi di suddivisione dell'area, e la verifico nel plot
 delta3 <- unsuperClass(delta, nClasses=3)
 
-cl <- colorRampPalette(c("blue", "green", "yellow"))(100)
+cl <- colorRampPalette(c("yellow", "blue", "green"))(100)
 plot(delta3$map, col=cl)
 
 # Creo un unico Multiframe con la visione in spettro visibile e la classificazione.
 par(mfrow=c(1,2))
-plotRGB(delta, r=4, g=2, b=1, stretch="lin")
+plotRGB(delta, r=4, g=3, b=2, stretch="lin")
 plot(delta3$map, col=cl)
 
 # Salvo l'immagine creata
 pdf("multi_VIS_class.pdf")
 par(mfrow=c(1,2))
-plotRGB(delta, r=4, g=2, b=1, stretch="lin")
+plotRGB(delta, r=4, g=3, b=2, stretch="lin")
 plot(delta3$map, col=cl)
 
 # Vado a calcolare le Frequenze delle classi
 freq(delta3$map)
      value    count
-[1,]     1 13223471
-[2,]     2  9878369
-[3,]     3  7038260
+[1,]     1  7827975
+[2,]     2 13290500
+[3,]     3  9021625
 
-# Dalle frequenze si nota come l'area considerata è prevalentemente coperta da acqua,
+# Dalle frequenze si nota come l'area considerata è prevalentemente coperta da acqua (classe 2),
 # in particolar modo a causa della presenza dell'Adriatico. Per quanto riguarda suolo incolto e aree
-# vegetate (agricoltura e pinete) hanno una frequenza abbastanza simile fra loro.
+# vegetate (agricoltura e pinete), rispettivamente classe 1 e classe 3, hanno una frequenza abbastanza simile fra loro.
 
 # Per visionare la percentuale occupata dalle varie classi:
 totdelta <- 30140100
-perc_acq <- (13223471*100)/totdelta
-perc_veg <- (9878369*100)/totdelta
+perc_acq <- (13290500*100)/totdelta
+perc_veg <- (9021625*100)/totdelta
 perc_suo <- 100 - (perc_acq + perc_veg)
 
 # Credo un Dataframe per sistemare i risultati
 Class <- c("Water", "Vegetation", "Soil")
-Percent <- c(43.87, 32.78, 23.35)
+Percent <- c(44.1, 25.97, 29.93)
 data <- data.frame(Class, Percent)
 
 # Risultato
        Class Percent
-1      Water   43.87
-2 Vegetation   32.78
-3       Soil   23.35
+1      Water   44.10
+2 Vegetation   25.97
+3       Soil   29.93
 
 # Vado ora a verificare la variabilità dell'area di interesse.
 
